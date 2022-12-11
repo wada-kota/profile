5d7d919233e80a45f4fd61f7433f7f8e359f1c37
Wada Kota <120293576+wada-kota@users.noreply.github.com>
2022-12-11 14:31:46 +0900

    first commit

---
+++ asset/js/index.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+$(function(){
+  $('.top-menu-btn').on("click", function(){
+    $('.top-btn-bar').toggleClass("active");
+    $('.top-menu').toggleClass("active");
+  })
+});
